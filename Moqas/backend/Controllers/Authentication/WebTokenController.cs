6d78e15cd08c8a07d734cd3a231e213cc63a670e
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2023-10-10 13:41:08 +0330

    add web token controller vanilla

---
+++ Moqas/backend/Controllers/Authentication/WebTokenController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Moqas.Controllers.Authentication
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebTokenController : ControllerBase
+    {
+    }
+}
