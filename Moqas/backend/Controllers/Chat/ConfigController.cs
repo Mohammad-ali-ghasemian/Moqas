3c97e6f580e74431d7f206cd6f61544ecb5637f8
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-05-27 11:16:21 +0330

    Create ConfigController.cs

---
+++ Moqas/backend/Controllers/Chat/ConfigController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Moqas.Controllers.Chat
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigController : ControllerBase
+    {
+    }
+}
