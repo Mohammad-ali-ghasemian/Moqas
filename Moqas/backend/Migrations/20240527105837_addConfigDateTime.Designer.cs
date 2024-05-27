2b998dadf89b056a46d644cabcfaf2fece495587
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-05-27 14:29:35 +0330

    add ConfigCreatedAt property to Customer Table

--- Moqas/backend/Migrations/20240527105837_addConfigDateTime.Designer.cs
+++ Moqas/backend/Migrations/20240527105837_addConfigDateTime.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Moqas.Model.Data;
 
@@ -11,9 +12,10 @@ using Moqas.Model.Data;
 namespace Moqas.Migrations
 {
     [DbContext(typeof(MoqasContext))]
-    partial class CustomerContextModelSnapshot : ModelSnapshot
+    [Migration("20240527105837_addConfigDateTime")]
+    partial class addConfigDateTime
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
