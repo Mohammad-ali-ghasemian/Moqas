904320523e44c519324eef6b665da2d49ae4614a
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-05-27 14:56:08 +0330

    update Profile Model

--- Moqas/backend/Model/Profile/Profile.cs
+++ Moqas/backend/Model/Profile/Profile.cs
@@ -2,8 +2,17 @@
 {
     public class Profile
     {
-        public int CustomerId { get; set; }
+        public int Id { get; set; }
         public string Email { get; set; } = string.Empty;
+        public string WebsiteLink { get; set; } = string.Empty;
+        public string ConfigUsername { get; set; } = string.Empty;
+        public DateTime? ConfigCreatedAt { get; set; }
+        public DateTime? ConfigExpires { get; set; }
+        public string? VerificationToken { get; set; }
         public DateTime? VerifiedAt { get; set; }
+        public string? PasswordResetToken { get; set; }
+        public DateTime? ResetTokenExpires { get; set; }
+        public string? BrowserToken { get; set; }
+        public DateTime? BrowserTokenExpires { get; set; }
     }
 }
