f00b06a8c3c426bb00b38013360d2caa6dbdbbea
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-05-27 14:58:01 +0330

    reArrange Profile Model

--- Moqas/backend/Model/Profile/Profile.cs
+++ Moqas/backend/Model/Profile/Profile.cs
@@ -4,15 +4,15 @@
     {
         public int Id { get; set; }
         public string Email { get; set; } = string.Empty;
-        public string WebsiteLink { get; set; } = string.Empty;
-        public string ConfigUsername { get; set; } = string.Empty;
-        public DateTime? ConfigCreatedAt { get; set; }
-        public DateTime? ConfigExpires { get; set; }
+        public string WebsiteLink { get; set; } = string.Empty;        
         public string? VerificationToken { get; set; }
         public DateTime? VerifiedAt { get; set; }
         public string? PasswordResetToken { get; set; }
         public DateTime? ResetTokenExpires { get; set; }
         public string? BrowserToken { get; set; }
         public DateTime? BrowserTokenExpires { get; set; }
+        public string ConfigUsername { get; set; } = string.Empty;
+        public DateTime? ConfigCreatedAt { get; set; }
+        public DateTime? ConfigExpires { get; set; }
     }
 }
