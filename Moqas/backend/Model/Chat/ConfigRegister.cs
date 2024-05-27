21ac2ca59cef79604f2d168a863ccf41bf06bf2a
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-05-27 11:48:17 +0330

    Rearrange ConfigRegister Properties

--- Moqas/backend/Model/Chat/ConfigRegister.cs
+++ Moqas/backend/Model/Chat/ConfigRegister.cs
@@ -5,12 +5,12 @@ namespace Moqas.Model.Authentication
     public class ConfigRegister
     {
         [Required]
+        public string BrowserToken { get; set; } = string.Empty;
+        [Required]
         public string Username { get; set; } = string.Empty;
         [Required, MinLength(6)]
         public string Password { get; set; } = string.Empty;
         [Required, Compare("Password")]
         public string ConfirmPassword { get; set; } = string.Empty;
-        [Required]
-        public string BrowserToken { get; set;} = string.Empty;
     }
 }
