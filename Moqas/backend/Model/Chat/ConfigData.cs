2845c309633f59ed3d7ecf5ae2116153006aedff
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-05-27 14:40:24 +0330

    add ConfigData Model

---
+++ Moqas/backend/Model/Chat/ConfigData.cs
@@ -0,0 +1,13 @@
+﻿namespace Moqas.Model.Chat
+{
+    public class ConfigData
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string BrowserToken { get; set; } = string.Empty;
+        public string WebsiteLink { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public DateTime? ConfigCreatedAt { get; set; }
+        public DateTime? ConfigExpires { get; set; }
+    }
+}
