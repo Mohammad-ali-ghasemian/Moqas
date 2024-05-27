58b5017caac13a7a48c359f4c8e6ea725d98c688
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-05-27 11:38:56 +0330

    Update ConfigController.cs

--- Moqas/backend/Controllers/Chat/ConfigController.cs
+++ Moqas/backend/Controllers/Chat/ConfigController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Moqas.Model.Authentication;
 using Moqas.Model.Data;
+using Moqas.Service.Authentication;
+using Moqas.Service.Chat;
 
 namespace Moqas.Controllers.Chat
 {
@@ -14,6 +17,10 @@ namespace Moqas.Controllers.Chat
             _context = context;
         }
 
-
+        [HttpGet("config-register")]
+        public async Task<IActionResult> ConfigRegister(CustomerRegister request)
+        {
+            return await ConfigService.ConfigRegister(this, _context, request);
+        }
     }
 }
