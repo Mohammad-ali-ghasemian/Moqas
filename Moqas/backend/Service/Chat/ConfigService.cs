c224e199b858103fe283709e5f4969b81a329075
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-05-27 11:40:36 +0330

    Create ConfigService.cs

---
+++ Moqas/backend/Service/Chat/ConfigService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moqas.Model.Authentication;
+using Moqas.Model.Data;
+
+namespace Moqas.Service.Chat
+{
+    public class ConfigService
+    {
+        public async static Task<IActionResult> ConfigRegister(ControllerBase controller, MoqasContext context, ConfigRegister request)
+        {
+
+        }
+    }
+}
