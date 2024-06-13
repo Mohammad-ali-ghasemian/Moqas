34380a29203c9541359339643beb5e4c96a7b434
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-06-13 21:45:41 +0330

    Update CustomerSettingsService.cs

--- Moqas/backend/Service/Chat/CustomerSettingsService.cs
+++ Moqas/backend/Service/Chat/CustomerSettingsService.cs
@@ -1,6 +1,24 @@
-﻿namespace Moqas.Service.Chat
+﻿using Microsoft.AspNetCore.Mvc;
+using Moqas.Model.Chat;
+using Moqas.Model.Data;
+
+namespace Moqas.Service.Chat
 {
     public class CustomerSettingsService
     {
+        public async static Task<IActionResult> InsertSetting(ControllerBase controller, MoqasContext context, CustomerSettings settings)
+        {
+            if (context.Customers.FirstOrDefault(u => u.Id == settings.CustomerId) == null)
+            {
+                return controller.BadRequest("There is no such customer id!");
+            }
+            context.CustomerSettings.Add(settings);
+            try
+            {
+                context.SaveChangesAsync();
+            }
+            catch (ObjectDisposedException ex) { }
+            return controller.Ok(settings.Id);
+        }
     }
 }
