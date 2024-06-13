98fc3922c59454561ee3b627ec5a2f4a8b8b95d8
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-06-13 21:48:59 +0330

    Update GetCustomerSettings.cs

--- Moqas/backend/Model/Chat/GetCustomerSettings.cs
+++ Moqas/backend/Model/Chat/GetCustomerSettings.cs
@@ -1,6 +1,12 @@
-﻿namespace Moqas.Model.Chat
+﻿using static Moqas.Model.Data.CustomerSettings;
+
+namespace Moqas.Model.Chat
 {
     public class GetCustomerSettings
     {
+        public int CustomerId { get; set; }
+        public TypeStyle Type { get; set; }
+        public string? Key { get; set; } = string.Empty;
+        public string? Value { get; set; } = string.Empty;
     }
 }
