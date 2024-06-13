b73eb47d78368ac9b5daa7445db2caa26d8fc6fb
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-06-13 21:34:20 +0330

    add CustomerSettings API & InsertSetting endpoint

---
+++ Moqas/backend/Controllers/Chat/CustomerSettingsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moqas.Model.Chat;
+using Moqas.Model.Data;
+using Moqas.Service.Chat;
+
+namespace Moqas.Controllers.Chat
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerSettingsController : ControllerBase
+    {
+        MoqasContext _context;
+        public CustomerSettingsController(MoqasContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("insert-setting")]
+        public async Task<IActionResult> InsertSetting(CustomerSettings settings)
+        {
+            return await CustomerSettingsService.InsertSetting(this, _context, settings);
+        }
+    }
+}
