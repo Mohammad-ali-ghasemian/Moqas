6aa46e189a2391eb53e936486b1b0b48744f48ea
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-06-15 08:48:56 +0330

    InsertDefaultCustomerSettings

--- Moqas/backend/Service/Chat/ConfigService.cs
+++ Moqas/backend/Service/Chat/ConfigService.cs
@@ -2,6 +2,7 @@
 using Moqas.Model.Chat;
 using Moqas.Model.Data;
 using Moqas.Service.Authentication;
+using System.Collections;
 
 namespace Moqas.Service.Chat
 {
@@ -45,8 +46,47 @@ namespace Moqas.Service.Chat
             }
             catch (ObjectDisposedException ex) { }
 
+            InsertDefaultCustomerSettings(context, customer.Id);
+
             return controller.Ok(customer.Id);
         }
+
+        public async static void InsertDefaultCustomerSettings(MoqasContext context, int customerId)
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("icon_backgroundColor", "#8e44ad");
+            hashtable.Add("icon", "CommentOutlined");
+            hashtable.Add("iconColor", "#ecf0f1");
+            hashtable.Add("size", "medium");
+            hashtable.Add("position", "left");
+            hashtable.Add("shape", "circle");
+            hashtable.Add("title", "");
+            hashtable.Add("status", "");
+            hashtable.Add("intro", "");
+            hashtable.Add("showFaqFirst", "true");
+            hashtable.Add("requireUsername", "false");
+            hashtable.Add("requirePhone", "false");
+            hashtable.Add("chat_background", "#ecf0f1");
+            ICollection keys = hashtable.Keys;
+
+            foreach(string item in keys)
+            {
+                context.CustomerSettings.Add(new CustomerSettings
+                {
+                    CustomerId = customerId,
+                    Type = "UI",
+                    Key = item,
+                    Value = (string) hashtable[item]
+                });
+            }
+
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (ObjectDisposedException ex) { }
+        }
+
 
         public async static Task<IActionResult> GetConfig(ControllerBase controller, MoqasContext context, string browserToken)
         {
