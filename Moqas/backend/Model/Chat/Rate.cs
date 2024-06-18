6777adb5046701aa753a0419148d662a2809a505
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-06-19 00:15:52 +0330

    Create Rate.cs

---
+++ Moqas/backend/Model/Chat/Rate.cs
@@ -0,0 +1,11 @@
+﻿namespace Moqas.Model.Chat
+{
+    public class Rate
+    {
+        public int rate1 { get; set; }
+        public int rate2 { get; set; }
+        public int rate3 { get; set; }
+        public int rate4 { get; set; }
+        public int rate5 { get; set; }
+    }
+}
