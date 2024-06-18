5a251a999eccfc9e92a00f759079683922571aa7
Mohammad-ali-ghasemian <m.aligh9work@gmail.com>
2024-06-18 23:33:20 +0330

    add Rate feature to database

---
+++ Moqas/backend/Migrations/20240618200201_addRate.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Moqas.Migrations
+{
+    public partial class addRate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rate",
+                table: "Chats",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rate",
+                table: "Chats");
+        }
+    }
+}
