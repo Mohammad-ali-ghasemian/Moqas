4ecf33daf1ea5bfdc09ccceb16ee7161ca9d47cd
Alireza Mousavi <alir.msvi@gmail.com>
2024-06-28 22:15:32 +0330

    add 3001 port

--- Moqas/backend/Program.cs
+++ Moqas/backend/Program.cs
@@ -7,7 +7,10 @@ builder.Services.AddCors(options => {
     options.AddPolicy("AllowLocalhost3000",
         builder =>
         {
-            builder.WithOrigins("http://localhost:3000", "https://localhost:3000")
+            builder.WithOrigins("http://localhost:3000", 
+                                "https://localhost:3000", 
+                                "http://localhost:3001", 
+                                "https://localhost:3001")
                    .AllowAnyMethod()
                    .AllowAnyHeader()
                    .AllowCredentials();
